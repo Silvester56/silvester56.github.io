dd36fdbe125a0770d6b9fbff921ad6ba2e6c1abc
silvester <silvester>
2021-03-30 14:38:07 +0200

    Make gridmove configurable

--- gridmoves.js
+++ gridmoves.js
@@ -1,10 +1,4 @@
 const edge = 7;
-let gridArray = [];
-for (var i = edge; i >= -edge; i--) {
-  for (var j = -edge; j <= edge; j++) {
-    gridArray.push({x: j, y: i});
-  }
-}
 
 let manhattan = (x, y) => {
   return Math.abs(x) + Math.abs(y);
@@ -19,18 +13,25 @@ let minkowski = (x, y) => {
 };
 
 let displayGrid = () => {
-  let grids = document.getElementsByClassName('grid');
-  for (var j = 0; j < grids.length; j++) {
-    for (var i = 0; i < gridArray.length; i++) {
-      let node = document.createElement("div");
-      let distance = eucledian(gridArray[i].x, gridArray[i].y);
-      node.classList.add("square");
-      if (distance <= j + 1) {
-        node.classList.add("active");
-      }
-      grids[j].appendChild(node);
+  let grid = document.getElementById('grid');
+  let edge = document.getElementById('size').value;
+  let move = document.getElementById('move').value;
+  document.documentElement.style.setProperty('--rows', edge * 2 + 1);
+  let gridArray = [];
+  for (var i = edge; i >= -edge; i--) {
+    for (var j = -edge; j <= edge; j++) {
+      gridArray.push({x: j, y: i});
     }
   }
+  grid.innerHTML = "";
+  let type = document.getElementById('type').value;
+  for (var i = 0; i < gridArray.length; i++) {
+    let node = document.createElement("div");
+    let distance = eval(type + "(gridArray[i].x, gridArray[i].y)");
+    node.classList.add("square");
+    if (distance <= move) {
+      node.classList.add("active");
+    }
+    grid.appendChild(node);
+  }
 };
-
-window.onload = displayGrid;
