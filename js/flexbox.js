abbf6954dce79839384df88e12d70535a5126ef3
silvester <silvester>
2021-04-05 23:28:27 +0200

    Add inputs for flexbox page

--- js/flexbox.js
+++ js/flexbox.js
@@ -1,4 +1,5 @@
-const selectors = ["flex-direction", "flex-wrap"];
+const selectors = ["flex-direction", "flex-wrap", "justify-content"];
+let nbItems = 3;
 
 window.onload = () => {
   for (var selector of selectors) {
@@ -14,4 +15,28 @@ let changeRules = () => {
     str = str + selector + ":" + document.getElementById(selector).value + ";";
   }
   document.getElementById("container").setAttribute("style", str);
+}
+
+let checkDisabled = () => {
+  document.getElementById('remove').disabled = (nbItems === 0);
+}
+
+let draw = () => {
+  let str = ""
+  for (var i = 0; i < nbItems; i++) {
+    str = `${str}<div class="item">${i + 1}</div>`;
+  }
+  document.getElementById("container").innerHTML = str;
+}
+
+let add = () => {
+  nbItems++;
+  draw();
+  checkDisabled();
+}
+
+let remove = () => {
+  nbItems--;
+  draw();
+  checkDisabled();
 }
