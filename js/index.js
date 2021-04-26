d4415f7c9996fb53ae1d7b5af90d50debec1d4b6
silvester <silvester>
2021-04-26 17:20:56 +0200

    Add more gifs

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 window.onload = () => {
   let img = document.getElementById('gif');
-  let number = Date.now() % 4;
+  let number = Date.now() % 9;
   img.setAttribute("src", `img/${number}.gif`);
 }
