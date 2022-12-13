33d8ecf1dc103bb889d54b8acc17ce74f64c9f32
silvester <silvester>
2022-12-13 19:59:29 +0100

    Add input on password page to exclude characters

--- js/passwords.js
+++ js/passwords.js
@@ -16,12 +16,13 @@ const generatePassword = () => {
   let uppercaseLettersAllowed = new domNode("#uppercaseLetters").prop("checked");
   let digitsAllowed = new domNode("#digits").prop("checked");
   let specialsAllowed = new domNode("#specials").prop("checked");
+  let exclude = new domNode("#exclude").val().split("");
   let result = [];
   while (result.length < passwordLength) {
-    result = result.concat(randomElements(letters.split(""), passwordLength / 4, lettersAllowed));
-    result = result.concat(randomElements(uppercaseLetters.split(""), passwordLength / 4, uppercaseLettersAllowed));
-    result = result.concat(randomElements(digits.split(""), passwordLength / 4, digitsAllowed));
-    result = result.concat(randomElements(specials.split(""), passwordLength / 4, specialsAllowed));
+    result = result.concat(randomElements(letters.split("").filter(c => !exclude.includes(c)), passwordLength / 4, lettersAllowed));
+    result = result.concat(randomElements(uppercaseLetters.split("").filter(c => !exclude.includes(c)), passwordLength / 4, uppercaseLettersAllowed));
+    result = result.concat(randomElements(digits.split("").filter(c => !exclude.includes(c)), passwordLength / 4, digitsAllowed));
+    result = result.concat(randomElements(specials.split("").filter(c => !exclude.includes(c)), passwordLength / 4, specialsAllowed));
   }
   if (!(new domNode("#assemble").prop("checked"))) {
     result = result.sort(() => 0.5 - Math.random());
