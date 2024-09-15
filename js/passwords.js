c425afc86712dd70a8e5a96a82e6071ca30d8d5a
silvester <silvester>
2024-09-16 01:23:22 +0200

    Better algorithm for password generation

--- js/passwords.js
+++ js/passwords.js
@@ -1,34 +1,36 @@
-const letters = "azertyuiopqsdfghjklmwxcvbn";
-const uppercaseLetters = letters.toUpperCase();
+const lowercaseletters = "azertyuiopqsdfghjklmwxcvbn";
+const uppercaseLetters = lowercaseletters.toUpperCase();
 const digits = "0123456789";
 const specials = "-!#$%&()*,./:;?@[]^_{|}~+<=>";
 
-const randomElements = (characterArray, segmentLength, allowed) => {
+const randomElement = (characterArray, allowed) => {
   if (!allowed) {
-    return "";
+    return [];
   }
-  return Array.from({length: segmentLength}, () => characterArray[Math.floor(Math.random() * characterArray.length)]);
+  return [characterArray[Math.floor(Math.random() * characterArray.length)]];
 };
 
 const generatePassword = () => {
   let passwordLength = new domNode("#length").val();
-  let lettersAllowed = new domNode("#letters").prop("checked");
+  let lowercaselettersAllowed = new domNode("#lowercaseletters").prop("checked");
   let uppercaseLettersAllowed = new domNode("#uppercaseLetters").prop("checked");
   let digitsAllowed = new domNode("#digits").prop("checked");
   let specialsAllowed = new domNode("#specials").prop("checked");
   let exclude = new domNode("#exclude").val().split("");
   let result = [];
-  while (result.length < passwordLength) {
-    result = result.concat(randomElements(letters.split("").filter(c => !exclude.includes(c)), passwordLength / 4, lettersAllowed));
-    result = result.concat(randomElements(uppercaseLetters.split("").filter(c => !exclude.includes(c)), passwordLength / 4, uppercaseLettersAllowed));
-    result = result.concat(randomElements(digits.split("").filter(c => !exclude.includes(c)), passwordLength / 4, digitsAllowed));
-    result = result.concat(randomElements(specials.split("").filter(c => !exclude.includes(c)), passwordLength / 4, specialsAllowed));
+  while (result.length < passwordLength && (lowercaselettersAllowed || uppercaseLettersAllowed || digitsAllowed || specialsAllowed))  {
+    result = result.concat(randomElement(lowercaseletters.split("").filter(c => !exclude.includes(c)), lowercaselettersAllowed));
+    result = result.concat(randomElement(uppercaseLetters.split("").filter(c => !exclude.includes(c)), uppercaseLettersAllowed));
+    result = result.concat(randomElement(digits.split("").filter(c => !exclude.includes(c)), digitsAllowed));
+    result = result.concat(randomElement(specials.split("").filter(c => !exclude.includes(c)), specialsAllowed));
   }
-  if (!(new domNode("#assemble").prop("checked"))) {
+  if (new domNode("#assemble").prop("checked")) {
+    result = result.sort((a, b) => (lowercaseletters + uppercaseLetters + digits + specials).split("").indexOf(a) - (lowercaseletters + uppercaseLetters + digits + specials).split("").indexOf(b));
+  } else {
     result = result.sort(() => 0.5 - Math.random());
   }
   new domNode("#password").val(result.slice(0, passwordLength).join(""));
-  navigator.clipboard.writeText(new domNode("#password").val()).then(() => new domNode("#copy").html("Copy"));
+  new domNode("#copy").html("Copy");
 };
 
 const copyPassword = () => {
