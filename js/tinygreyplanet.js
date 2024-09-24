464f90d356cf66cedab6a881da9dede742f3fd0e
silvester <silvester>
2024-09-24 12:24:29 +0200

    Tinygreyplanet : add robot sprite and craters

--- js/tinygreyplanet.js
+++ js/tinygreyplanet.js
@@ -8,6 +8,8 @@ window.onload = () => {
   const polarCenterX = world.width / 2;
   const polarCenterY = world.height * 2;
 
+  let robotSprite = new Image();
+
   const keys = {
     ArrowLeft: { pressed: false },
     ArrowRight: { pressed: false },
@@ -16,12 +18,13 @@ window.onload = () => {
     Space: { pressed: false }
   }
 
-  class Planet {
-    constructor(r, a, s, c) {
+  class RoundObject {
+    constructor(r, a, s, c, f = true) {
       this.centerRadius = r;
       this.centerAngle = a;
       this.size = s;
       this.color = c;
+      this.fill = f;
     }
 
     update() {
@@ -35,9 +38,13 @@ window.onload = () => {
     draw() {
       canvasContext.beginPath();
       canvasContext.arc(polarCenterX + this.centerRadius * Math.cos(this.centerAngle), polarCenterY + this.centerRadius * Math.sin(this.centerAngle), this.size, 0, 2 * Math.PI, false);
-      canvasContext.stroke();
-      canvasContext.fillStyle = this.color;
-      canvasContext.fill();
+      if (this.fill) {
+        canvasContext.fillStyle = this.color;
+        canvasContext.fill();
+      } else {
+        canvasContext.strokeStyle = this.color;
+        canvasContext.stroke();
+      }
     }
   }
 
@@ -52,38 +59,52 @@ window.onload = () => {
   class Player extends Person {
     constructor(r, a) {
       super(r, a);
-      this.drawWidth = 32;
-      this.drawHeight = 32;
+      this.drawWidth = 27;
+      this.drawHeight = 33;
+      this.spriteOffsetX = 0;
+      this.spriteOffsetY = 0;
+      this.spriteCroppedWidth = 27;
+      this.spriteCroppedHeight = 33;
     }
 
     draw() {
-      canvasContext.fillStyle = "red";
-      canvasContext.fillRect(polarCenterX + this.positionRadius * Math.cos(this.positionAngle) - this.drawWidth / 2, polarCenterY + this.positionRadius * Math.sin(this.positionAngle) - this.drawHeight, this.drawWidth, this.drawHeight);
+      if (robotSprite) {
+        canvasContext.drawImage(robotSprite, this.spriteOffsetX, this.spriteOffsetY, this.spriteCroppedWidth, this.spriteCroppedHeight, polarCenterX + this.positionRadius * Math.cos(this.positionAngle) - this.drawWidth / 2, polarCenterY + this.positionRadius * Math.sin(this.positionAngle) - this.drawHeight, this.drawWidth, this.drawHeight);
+      }
     }
 
     update() {
+      if (keys.ArrowUp.pressed) {
+        this.velocityRadius = 2;
+      }
+      if (keys.ArrowLeft.pressed) {
+        this.spriteOffsetX = 27;
+      } else if (keys.ArrowRight.pressed) {
+        this.spriteOffsetX = 0;
+      }
       this.positionRadius = this.positionRadius + this.velocityRadius;
-      if (this.positionRadius > polarCenterY / 1.5) {
+      if (this.positionRadius > polarCenterY / 1.5) { // TODO polarCenterY / 1.5 must be a variable in constructor
         this.velocityRadius = this.velocityRadius - 0.1;
       } else {
         this.positionRadius = polarCenterY / 1.5;
         this.velocityRadius = 0;
       }
     }
-
-    jump() {
-      this.velocityRadius = 2;
-    }
   }
   
-  const planet = new Planet(0, 0, polarCenterY / 1.5, "grey")
-  const stars = Array.from(Array(100), (_, number) => new Planet(polarCenterY / 1.5 + number * 5 * Math.random(), Math.random() * 2 * Math.PI, 1, "white"));
+  const planet = new RoundObject(0, 0, polarCenterY / 1.5, "grey")
+  const craters = Array.from(Array(100), (_, number) => new RoundObject(polarCenterY / 1.5 - number * 5 * Math.random(), Math.random() * 2 * Math.PI, 20 * Math.random(), "black", Math.random() > 0.5));
+  const stars = Array.from(Array(100), (_, number) => new RoundObject(polarCenterY / 1.5 + number * 5 * Math.random(), Math.random() * 2 * Math.PI, 1, "white"));
   const player = new Player(polarCenterY / 1.5, Math.PI * 3 / 2);
 
   const animationLoop = () => {
     requestAnimationFrame(animationLoop);
     canvasContext.clearRect(0, 0, world.width, world.height);
     planet.draw();
+    craters.forEach(c => {
+      c.update();
+      c.draw()
+    });
     stars.forEach(s => {
       s.update();
       s.draw()
@@ -103,9 +124,8 @@ window.onload = () => {
       case 'ArrowUp':
       keys.ArrowUp.pressed = true;
       break;
-      case 'ArrowDown':
-      keys.ArrowDown.pressed = true;
-      break;
+      case ' ':
+      keys.Space.pressed = false; // TODO same in radima
     }
   });
 
@@ -120,13 +140,13 @@ window.onload = () => {
       case 'ArrowUp':
       keys.ArrowUp.pressed = false;
       break;
-      case 'ArrowDown':
-      keys.ArrowDown.pressed = false;
-      break;
       case ' ':
-      player.jump();
+      keys.Space.pressed = false; // TODO same in radima
     }
   });
 
-  animationLoop();
+  robotSprite.src = "../img/robot.png";
+  robotSprite.onload = () => {
+    animationLoop();
+  }
 }
