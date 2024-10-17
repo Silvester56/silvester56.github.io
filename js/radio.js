9c1e3188d77de75ef4bc88baa42e28c95be2f67e
silvester <silvester>
2024-10-17 15:12:10 +0200

    Radio : improve audio handling and time calculation

--- js/radio.js
+++ js/radio.js
@@ -1,20 +1,20 @@
 window.onload = () => {
-  let audio = document.querySelector('#mix');
+  const audio = new Audio("../audio/mix.mp3");
+  audio.loop = true;
+  audio.volume = 0.2;
+
   document.querySelector('#volume').onchange = function() {
     audio.volume = this.value / 100;
   };
-  let volume = 20;
-  audio.volume = volume / 100;
-
-  playOrStopRadio = () => {
+  
+  document.querySelector("#control").onclick = () => {
     if (audio.paused) {
-      let time = new Date();
-      audio.currentTime = time.getMinutes() * 60 + time.getSeconds();
+      audio.currentTime = Math.floor(Date.now() / 1000) % Math.floor(audio.duration);
       audio.play();
       document.querySelector("#control").innerHTML = "Stop";
     } else {
       audio.pause();
       document.querySelector("#control").innerHTML = "Play";
     }
-  }
+  };
 }
