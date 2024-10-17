ba069b5501f3eee9c6e58590c3528d4a4843b19a
silvester <silvester>
2024-10-17 16:10:26 +0200

    Flexbox : improve CSS rules creation

--- js/flexbox.js
+++ js/flexbox.js
@@ -1,36 +1,32 @@
 const selectors = ["#flex-direction", "#flex-wrap", "#justify-content", "#align-items", "#align-content"];
-let nbItems = 3;
+let numberOfItems = 3;
 let container;
 let removeButton;
+let addButton;
+
+const changeRules = () => {
+  let cssString = selectors.reduce((acc, cur) =>{
+    let element = document.querySelector(cur);
+    if (element.value !== "") {
+      return `${acc + cur.slice(1)}:${element.value};`;
+    }
+    return acc;
+  }, "");
+  container.style = cssString;
+};
+
+const changeItems = (nb) => {
+  numberOfItems = numberOfItems + nb;
+  container.innerHTML = Array.from(Array(numberOfItems)).map((_, i) => `<div class="item">${i + 1}</div>`).join("");
+  removeButton.disabled = numberOfItems === 0;
+};
 
 window.onload = () => {
-  container = new domNode("#container");
-  removeButton = new domNode("#remove");
+  container = document.querySelector("#container");
+  removeButton = document.querySelector("#removeItem");
+  addButton = document.querySelector("#addItem");
+  removeButton.onclick = () => changeItems(-1);
+  addButton.onclick = () => changeItems(1);
   changeRules();
-  for (var selector of selectors) {
-    new domNode(selector).on("change", (event) => {
-      changeRules();
-    });
-  }
-}
-
-let changeRules = () => {
-  let str = "";
-  for (var selector of selectors) {
-    let element = new domNode(selector);
-    if (element.val() !== "") {
-      str = `${str + selector.slice(1)}:${element.val()};`;
-    }
-  }
-  container.attr("style", str);
-}
-
-let changeItems = (nb) => {
-  let str = "";
-  nbItems = nbItems + nb;
-  for (var i = 0; i < nbItems; i++) {
-    str = `${str}<div class="item">${i + 1}</div>`;
-  }
-  container.html(str);
-  removeButton.prop("disabled", (nbItems === 0));
+  selectors.forEach(s => document.querySelector(s).onchange = changeRules);
 }
