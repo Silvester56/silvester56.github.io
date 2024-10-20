947548263d27f3ae50fadcf700a3d56b7dc32107
silvester <silvester>
2024-10-20 16:45:52 +0200

    Saboteur : clean html and js

--- js/saboteur.js
+++ js/saboteur.js
@@ -1,58 +1,36 @@
 let diggersArray = [];
 let saboteursArray = [];
 let playersArray = [];
-
 let possibleGames = [];
 
-let results;
+const checkDisabled = () => {
+  document.querySelector("#removeDigger").disabled = diggersArray.length === 0;
+  document.querySelector("#removeSaboteur").disabled = saboteursArray.length === 0;
+  document.querySelector("#removePlayer").disabled = playersArray.length === 0;
+  document.querySelector("#addPlayer").disabled = playersArray.length >= diggersArray.length + saboteursArray.length;
+  document.querySelector("#calculate").disabled = playersArray.length === 0 || diggersArray.length === 0 || saboteursArray.length === 0;
+};
 
-let checkDisabled = () => {
-  new domNode("#dminus").prop("disabled", diggersArray.length === 0);
-  new domNode("#sminus").prop("disabled", saboteursArray.length === 0);
-  new domNode("#pminus").prop("disabled", playersArray.length === 0);
-  new domNode("#pplus").prop("disabled", playersArray.length >= diggersArray.length + saboteursArray.length);
-  new domNode("#start").prop("disabled", playersArray.length === 0 || diggersArray.length === 0 || saboteursArray.length === 0);
-}
+const htmlFromArray = (arr, className) => arr.reduce((acc, cur) => `${acc}<div class="${className}">${cur}</div>`, "");
 
-window.onload = () => {
-  results = new domNode("#results");
-  checkDisabled();
-}
+const draw = () => {
+  document.querySelector("#cards").innerHTML = htmlFromArray(diggersArray, "digger") + htmlFromArray(saboteursArray, "saboteur");
+  document.querySelector("#players").innerHTML = htmlFromArray(playersArray, "player");
+};
 
-let htmlFromArray = (arr, className) => arr.reduce((acc, cur) => `${acc}<div class="${className}">${cur}</div>`, "")
-
-let draw = () => {
-  new domNode("#cards").html(htmlFromArray(diggersArray, "digger") + htmlFromArray(saboteursArray, "saboteur"));
-  new domNode("#players").html(htmlFromArray(playersArray, "player"));
-}
-
-let remove = (type) => {
-  if (type === 1) {
-    diggersArray.pop();
-  } else if (type === 2) {
-    saboteursArray.pop();
-  } else if (type === 3) {
-    playersArray.pop();
-  }
+const remove = (array) => {
+  array.pop();
   draw();
   checkDisabled();
-}
+};
 
-let add = (type) => {
-  let id;
-  if (type === 1) {
-    diggersArray.push(`D${diggersArray.length + 1}`);
-  } else if (type === 2) {
-    saboteursArray.push(`S${saboteursArray.length + 1}`);
-  } else if (type === 3) {
-    playersArray.push(`P${playersArray.length + 1}`);
-    id = "players";
-  }
+const add = (array, letter) => {
+  array.push(`${letter}${diggersArray.length + 1}`);
   draw();
   checkDisabled();
-}
+};
 
-let game = (str, cards, players) => {
+const game = (str, cards, players) => {
   if (players.length === 0) {
     possibleGames.push(str);
     return `${str} | `;
@@ -65,34 +43,45 @@ let game = (str, cards, players) => {
     tmp = tmp + game(str + p + cards[i], newCards, players.slice(1));
   }
   return tmp;
-}
+};
 
-let percentage = (sum, total) => {
+const percentage = (sum, total) => {
   let result = `${100 * sum / total}`;
   return `${result.substring(0, 5)}%`;
-}
+};
 
-let calculate = () => {
+const calculate = () => {
   let sNumbers = saboteursArray.map(x => 0);
   sNumbers.push(0);
   possibleGames = [];
   cArray = diggersArray.concat(saboteursArray);
   pArray = playersArray.map(x => x);
-  console.log(game("", cArray, pArray));
   for (var i = 0; i < possibleGames.length; i++) {
     let nb = possibleGames[i].split("S").length - 1;
     sNumbers[nb]++;
   }
-  results.html(sNumbers.reduce((acc, cur, index) => `${acc}<div>Distributions with exactly ${index} saboteurs : ${cur} (${percentage(cur, possibleGames.length)})</div>`, `<div>Possible distributions : ${possibleGames.length}</div>`));
-}
+  document.querySelector("#results").innerHTML = sNumbers.reduce((acc, cur, index) => `${acc}<div>Distributions with exactly ${index} saboteurs : ${cur} (${percentage(cur, possibleGames.length)})</div>`, `<div>Possible distributions : ${possibleGames.length}</div>`);
+};
 
-let reset = () => {
+const reset = () => {
   diggersArray = [];
   saboteursArray = [];
   playersArray = [];
   possibleGames = [];
-  new domNode("#cards").html("");
-  new domNode("#players").html("");
-  results.html("");
+  document.querySelector("#cards").innerHTML = "";
+  document.querySelector("#players").innerHTML = "";
+  document.querySelector("#results").innerHTML = "";
+  checkDisabled();
+};
+
+window.onload = () => {
+  document.querySelector("#addDigger").onclick = () => add(diggersArray, "D");
+  document.querySelector("#addSaboteur").onclick = () => add(saboteursArray, "S");
+  document.querySelector("#addPlayer").onclick = () => add(playersArray, "P");
+  document.querySelector("#removeDigger").onclick = () => remove(diggersArray);
+  document.querySelector("#removeSaboteur").onclick = () => remove(saboteursArray);
+  document.querySelector("#removePlayer").onclick = () => remove(playersArray);
+  document.querySelector("#calculate").onclick = calculate;
+  document.querySelector("#reset").onclick = reset;
   checkDisabled();
 }
