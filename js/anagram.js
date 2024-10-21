27d6322c04a75666485e6e67fd14aa32fe7e0af5
silvester <silvester>
2024-10-21 23:34:42 +0200

    Anagram : add a reverse button and a disable mechanism

--- js/anagram.js
+++ js/anagram.js
@@ -1,4 +1,4 @@
-let letterTab;
+let letterTab = [];
 let container;
 let input;
 
@@ -19,19 +19,34 @@ const draw = () => {
   letterTab.map(elementFromLetter).forEach(el => container.append(el));
 };
 
+const checkDisabled = () => {
+  document.querySelector("#reset").disabled = input.value.length === 0;
+  document.querySelector("#shuffle").disabled = letterTab.length === 0;
+  document.querySelector("#reverse").disabled = letterTab.length === 0;
+};
+
 window.onload = () => {
   container = document.querySelector("#container");
   input = document.querySelector("#word");
 
+  checkDisabled();
+  input.oninput = checkDisabled;
   document.querySelector("#reset").onclick = () => {
     letterTab = input.value.toLowerCase().split('').map(x => {
       return {letter: x, locked: false};
     });
+    checkDisabled();
     draw();
   };
 
   document.querySelector("#shuffle").onclick = () => {
     let toMove = letterTab.filter(el => !el.locked).sort(() => 0.5 - Math.random());
+    letterTab = letterTab.map(el => el.locked ? el : toMove.shift());
+    draw();
+  };
+
+  document.querySelector("#reverse").onclick = () => {
+    let toMove = letterTab.filter(el => !el.locked).reverse();
     letterTab = letterTab.map(el => el.locked ? el : toMove.shift());
     draw();
   };
